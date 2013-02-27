23337dcc7e0e7099d44e3976615113338e27fb1e
Janusz A. Urbanowicz <aurbanowicz@centrumcyfrowe.pl>
2013-02-27 14:12:22 +0100

    ujawnienie

--- afera/views.py
+++ afera/views.py
@@ -8,12 +8,29 @@ from django.db import connections
 
 # not views
 
-def scandal (scandal_id, db='afery'):
+def get_scandal (scandal_id, db='afery'):
 
   cursor = connections[db].cursor()
   cursor.execute("SELECT name,description,background  FROM scandals WHERE id=%s;", (scandal_id,))
   return cursor.fetchall()
-          
+
+def get_events (scandal_id, db='afery'):
+
+  ''' id  | description | scandal_id | location_id | event_date | publication_date | type_id | subtype_id | types | refs | title | description                                                                                                                                                                                                                                                                                     | scandal_id | location_id | event_date | publication_date | type_id | subtype_id | types | refs | title'''
+                
+  cursor = connections[db].cursor()
+  cursor.execute ("SELECT id, event_date, description, publication_date, types, refs FROM events WHERE scandal_id=%s ORDER BY event_date ASC;", (scandal_id,))
+
+  return cursor.fetchall ()
+
+def get_event_actors (event_id, db='afery'):
+
+  cursor = connections[db].cursor()
+  
+  cursor.execute ("SELECT actors.name,affiliations FROM actors_events JOIN actors ON actors_events.actor_id=actors.id where event_id=%s;",(event_id,))
+
+  return cursor.fetchall ()
+            
 # views
 
 
@@ -24,13 +41,25 @@ def case (request, object_id):
 #  cursor = connections['afery'].cursor()
 #  cursor = connections['kuku'].cursor()
 
-  data = scandal(object_id)
+  scandal = get_scandal(object_id)
+  events = get_events (object_id)
+
+#  print events[0]
+
+  ujawnienie = (events[0], get_event_actors(events[0][0]))
+
+  print ujawnienie
+
+    
   result = {} 
-  print data
   
-  result['name'] = data[0][0][0]
-  result['names'] = data[0][0][1:]
-  
+  result['name'] = scandal [0][0][0]
+  result['names'] = scandal [0][0][1:]
+  result['description'] = scandal [0][1]  
+  result['num_events'] = len(events)
+  result['event_leak'] = ujawnienie
+  result['events'] = events[1:]
+   
   template = loader.get_template ('afera.html')
 
   return HTTPResponse (template.render(Context(result))  )
