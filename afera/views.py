47c207b3bf29d5e08418fd164cdcda0d172805ec
Janusz A. Urbanowicz <aurbanowicz@centrumcyfrowe.pl>
2013-02-27 12:11:51 +0100

    external database

--- afera/views.py
+++ afera/views.py
@@ -4,11 +4,16 @@ from django.http import HttpResponse as HTTPResponse
 
 from django.template import Context, RequestContext, loader, Template
 
+from django.db import connections
+
 
 def case (request, object_id):
 
   'single scandal view'
   
+  cursor = connections['afery'].cursor()
+
+
   
   
   template = loader.get_template ('afera.html')
