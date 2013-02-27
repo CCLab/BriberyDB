ed24d17bb35b084891c19471e305d83c9c1a729f
Janusz A. Urbanowicz <aurbanowicz@centrumcyfrowe.pl>
2013-02-27 12:35:37 +0100

    titles work

--- afera/views.py
+++ afera/views.py
@@ -6,17 +6,32 @@ from django.template import Context, RequestContext, loader, Template
 
 from django.db import connections
 
+# not views
+
+def scandal (scandal_id, db='afery'):
+
+  cursor = connections[db].cursor()
+  cursor.execute("SELECT name,description,background  FROM scandals WHERE id=%s;", (scandal_id,))
+  return cursor.fetchall()
+          
+# views
+
 
 def case (request, object_id):
 
   'single scandal view'
   
-  cursor = connections['afery'].cursor()
+#  cursor = connections['afery'].cursor()
+#  cursor = connections['kuku'].cursor()
 
-
+  data = scandal(object_id)
+  result = {} 
+  print data
   
+  result['name'] = data[0][0][0]
+  result['names'] = data[0][0][1:]
   
   template = loader.get_template ('afera.html')
 
-  return HTTPResponse (template.render(Context({}))  )
+  return HTTPResponse (template.render(Context(result))  )
   
