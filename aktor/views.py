57a868d3ef9b368a3ee06a8ff6d244f9b7d5d6e3
Janusz A. Urbanowicz <aurbanowicz@centrumcyfrowe.pl>
2013-02-28 15:49:42 +0100

    event_actors defined

--- aktor/views.py
+++ aktor/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+
+import orm
+
+def actor (request, object_id):
+
+  pass
+
+
+def event_actors (request, object_id):
+
+  actors = orm.query('event_actors', object_id)
+
+  print actors
