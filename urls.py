96f2985f469a8a72bb6cb7f0cd18daef3f104ae0
Janusz A. Urbanowicz <aurbanowicz@centrumcyfrowe.pl>
2013-02-28 15:43:13 +0100

    static pages defined

--- urls.py
+++ urls.py
@@ -1,14 +1,23 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+
 import settings
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
+    (r'^$', 'direct_to_template', dict(template='main.html'), 'page_main'),
+    (r'^/o-serwisie/$', 'direct_to_template', dict(template='about.html'),'page_about'),
+    (r'^/kontakt/$', 'direct_to_template', dict(template='contact.html'),'page_contact'),
+     
     # (r'^afery/', include('afery.foo.urls')),
     
-    (r'afera/(?P<object_id>\d+)/', 'afera.views.case' ),
+    (r'afera/(?P<object_id>\d+)/$', 'afera.views.case' ),
+
+    
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
