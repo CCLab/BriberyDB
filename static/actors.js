a85c390f9b32647fa71ab9ba5222c17f5ca9dc36
root <root@centrumcyfrowe.pl>
2013-04-08 10:30:35 +0200

    case actors function

--- static/actors.js
+++ static/actors.js
@@ -1,4 +1,4 @@
-function actors (case_id) {
+function case_actors (case_id) {
 
     var xmlhttp;
     var url;
@@ -10,7 +10,33 @@ function actors (case_id) {
 	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
     }
 
-    url = '/afera/' + case_id + '/actors/';
+    url = '/afera/' + case_id + '/podmioty/';
+
+    xmlhttp.onreadystatechange = function() {
+
+	var popup = document.getElementById('wydarzenie-aktorzy-'+case_id)
+	if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+	    popup.innerHTML=xmlhttp.responseText;
+	}
+    }
+
+    xmlhttp.open('GET', url, true);
+    xmlhttp.send();
+}
+
+function event_actors (case_id) {
+
+    var xmlhttp;
+    var url;
+
+    if (window.XMLHttpRequest)
+    { // code for IE7+, Firefox, Chrome, Opera, Safari
+	xmlhttp=new XMLHttpRequest();
+    } else { // code for IE6, IE5
+	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    url = '/wydarzenie/' + case_id + '/podmioty/';
 
     xmlhttp.onreadystatechange = function() {
 
